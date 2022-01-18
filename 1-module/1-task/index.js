1dc0d6a713445950e3aef54b64122e7d06326573
Petr Zavadski <petrzavadski@gmail.com>
2022-01-18 13:33:30 +0300

    факториал

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,19 @@
 function factorial(n) {
-  // ваш код...
+ 
+  if (n==0) return 1
+if (n==1) return 1
+
+let res = n;
+
+for(let i=0; i<n-1; i++){
+
+
+res = res * (n-i-1)
+
+
+
+}
+
+return res
+
 }
