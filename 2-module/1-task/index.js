5f2eb95ad9c1ff87c3db6e479ea8c663aac1c998
Petr Zavadski <petrzavadski@gmail.com>
2022-01-21 13:25:42 +0300

    Решение задания task-3

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,18 @@
 function sumSalary(salaries) {
-  // ваш код...
+
+  let sum =[];
+
+  for (s in salaries){
+  
+  // console.log(salaries[s])
+  
+  if (typeof salaries[s]=='number') {
+  
+    sum.push(salaries[s]);
+  }
+  
+  }
+  
+  return sum.reduce((a, b)=> a+b)
+
 }
